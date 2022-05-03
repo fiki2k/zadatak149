e00b0f149160df066b345ef088ce7bbdc045495c
fiki2k <91737444+fiki2k@users.noreply.github.com>
2022-05-03 08:37:35 +0200

    Add files via upload

---
+++ zad149.c
@@ -0,0 +1,28 @@
+// Program treba zamjenit 2 broja koristeci pointere.
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int br1,br2;
+    int *a,*b,*temp;
+
+    printf("Unesi prvi broj: ");
+    scanf("%d",&br1);
+    printf("\nUnesi drugi broj: ");
+    scanf("%d",&br2);
+
+    a=&br1;
+    b=&br2;
+
+    printf("\nPrije zamjene :\n\n\ta = %d\tb = %d\n",*a,*b);
+
+    temp=a;
+    a=b;
+    b=temp;
+
+    printf("\nNakon zamjene :\n\n\ta = %d\tb = %d\n",*a,*b);
+
+    return 0;
+}
